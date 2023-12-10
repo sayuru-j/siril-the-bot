454fd77ff617b898741b4fa755e55d129fe9eaaf
Sayuru J <zayuru.dev@gmail.com>
2023-12-10 22:08:36 +0530

    modified:   client/components/chat.js 	modified:   client/components/header.js 	modified:   client/public/logo.png

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,7 +1,13 @@
 "use client";
 
 import { useTypingEffect } from "@/hooks/typing-effect";
-import { BotIcon, DotIcon, Edit2Icon, SendHorizontalIcon } from "lucide-react";
+import {
+  BotIcon,
+  CopyIcon,
+  DotIcon,
+  Edit2Icon,
+  SendHorizontalIcon,
+} from "lucide-react";
 import { useState } from "react";
 import Markdown from "react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
@@ -45,49 +51,67 @@ export default function Chat() {
 
   let reply = useTypingEffect(chatRes, 40);
 
+  async function copyToClipboard() {
+    try {
+      if (navigator.clipboard) {
+        await navigator.clipboard.writeText("Test");
+        console.log("Text copied to clipboard!");
+      } else {
+        console.log("Clipboard API not available in this environment.");
+      }
+    } catch (error) {
+      console.error("Error copying to clipboard:", error);
+    }
+  }
+
   return (
-    <div class="max-w-3xl mx-auto py-4 sm:px-0 px-4">
+    <div className="max-w-3xl mx-auto py-4 sm:px-0 px-4">
       <div className="flex flex-col gap-2 min-h-[85vh]">
         <div className="p-2 rounded-3xl relative">
           {prevMessage && sent && (
             <div>
               {prevMessage}
-              <div className="absolute right-2 top-2">
-                <Edit2Icon className="w-4" />
-              </div>
+              <button type="button" className="absolute right-2 top-2">
+                <Edit2Icon className="w-4 opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-200 ease-in-out" />
+              </button>
             </div>
           )}
         </div>
-        <div className="flex gap-2 border p-2 rounded-3xl overflow-y-auto">
-          <div className="pt-2">
-            <BotIcon />
+        <div className="flex gap-2 border p-5 rounded-3xl overflow-y-auto">
+          <div className="">
+            <BotIcon className="w-6 h-6" />
           </div>
-          <div className="p-2 rounded-3xl relative w-full">
+          <div className="rounded-3xl relative w-full">
             {reply ? (
-              <Markdown
-                className="flex flex-col gap-4 leading-relaxed"
-                children={reply}
-                components={{
-                  code(props) {
-                    const { children, className, node, ...rest } = props;
-                    const match = /language-(\w+)/.exec(className || "");
-                    return match ? (
-                      <SyntaxHighlighter
-                        className="rounded-xl"
-                        {...rest}
-                        PreTag="div"
-                        children={String(children).replace(/\n$/, "")}
-                        language={match[1]}
-                        style={docco}
-                      />
-                    ) : (
-                      <code {...rest} className={className}>
-                        {children}
-                      </code>
-                    );
-                  },
-                }}
-              />
+              <div className="relative">
+                <Markdown
+                  className="flex flex-col gap-4 leading-relaxed"
+                  children={reply}
+                  components={{
+                    code(props) {
+                      const { children, className, node, ...rest } = props;
+                      const match = /language-(\w+)/.exec(className || "");
+                      return match ? (
+                        <SyntaxHighlighter
+                          className="rounded-xl"
+                          {...rest}
+                          PreTag="div"
+                          children={String(children).replace(/\n$/, "")}
+                          language={match[1]}
+                          style={docco}
+                        />
+                      ) : (
+                        <code {...rest} className={className}>
+                          {children}
+                        </code>
+                      );
+                    },
+                  }}
+                />
+                <button onClick={copyToClipboard} type="button">
+                  <CopyIcon className="w-4 absolute bottom-0 right-0 opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-200 ease-in-out" />
+                </button>
+              </div>
             ) : (
               <div className="flex items-center space-x-1">
                 <h2>Hearing you</h2>
@@ -98,22 +122,26 @@ export default function Chat() {
         </div>
       </div>
 
-      <div className="w-full bottom-0 max-w-3xl mx-auto py-2 sticky bg-white">
-        <div className="flex items-center justify-center relative">
-          <input
-            className="w-full py-3 px-3 text-opacity-70 border rounded-full !outline-none"
-            placeholder="Ask Something!"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-              setSent(false);
-            }}
-            onKeyDown={(e) => {
-              if (e.key == "Enter") {
-                sendMessage();
-              }
-            }}
-          />
+      <div className="bottom-0 max-w-3xl mx-auto py-2 sticky bg-white">
+        <div className="flex items-center justify-center relative break-words hover:break-words">
+          <div className="w-full break-words hover:break-words">
+            {/* Make words break when it reaches the max width */}
+            <input
+              type="text"
+              className="w-full py-3 px-3 text-opacity-70 border rounded-full !outline-none focus:outline-none"
+              placeholder="Ask Something!"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+                setSent(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key == "Enter") {
+                  sendMessage();
+                }
+              }}
+            />
+          </div>
           <button
             onClick={sendMessage}
             type="button"
--- client/components/header.js
+++ client/components/header.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex items-center sm:justify-between justify-center w-full px-4 py-2 border-b-[1.5px]">
+    <header className="flex items-center sm:justify-between justify-center w-full px-4 py-4 border-b-[1.5px]">
       <div className="flex gap-1 items-center justify-center">
         <Image
-          className="w-6 antialiased"
+          className="w-20 antialiased"
           src="/logo.png"
           alt="siril-the-bot"
           width={100}
           height={100}
         />
-        <h1 className="font-black text-xl">SIRIL</h1>
+        <h1 className="font-black text-xl hidden">SIRIL</h1>
       </div>
       <div className="sm:flex items-center justify-center hidden">
         <FlaskConicalIcon className="opacity-50 hover:opacity-100 hover:cursor-pointer transition-all duration-300 ease-in-out" />
